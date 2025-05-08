1c26cea91a857a524d66b460fb97c85ad0521df9
sonv2 <sonv2@fpt.com>
2025-05-08 18:40:03 +0900

    'Move database connection string to environment variables'

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: "postgres://neondb_owner:npg_QPGhIV8ax4AF@ep-tight-cloud-a1oku90l-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+  connectionString: process.env.DB_CONNECTION_STRING
 });
 
 export default pool;
